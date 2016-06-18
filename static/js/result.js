6025ad49ff73eda7c56be480531aef20f02e7756
hyeinkkkk <hyein826@gmail.com>
2016-06-18 22:42:13 +0900

    자식 div에는 flex를 제거했다. (아이폰에서 깨져서)

--- static/js/result.js
+++ static/js/result.js
@@ -2,9 +2,8 @@ songApp.controller('ResultControllor', function($scope,$http,$location,$mdDialog
 {
     $scope.type = dataStorage.get();
 
-    // $scope.type = {id: 2, description: "느슨한 티셔츠 같던 우리 사이를 해결해줄 조개롱", name: "조개롱"}
+    // $scope.type = {id: 2, description: "선택은 한번도 내 몫인 적이 없는데, 채 꺼내어먹지도 않앗던 마다가스카르 바닐라빈 화고롱에. 넌 지키고 싶은게 너무나 많았기에 와고롱을 양보할 생각이 없었나봐", name: "마다가스카르 바닐라빈 와고롱"}
     console.log("player Type???? ", $scope.type);
-
 
     $scope.goHome = function(){
         $location.path("/");
