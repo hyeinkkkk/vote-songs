7427256e0e5387cd3221e87ad99ef83d78c6a2b1
hyeinkkkk <hyein826@gmail.com>
2016-06-24 10:30:30 +0900

    dom 직접 건드려보자

--- app/static/js/intro.js
+++ app/static/js/intro.js
@@ -4,7 +4,13 @@ songApp.controller('IntroControllor', function($scope,$http,$location)
         $location.path("/personal-data");
     };
 
-    angular.element("#letter").css("color","white");
+    // angular.element("#letter").css("color","white");
+
+    var d = document.getElementById("letter1");
+    d.setAttribute("style","color:white");
+
+    var d2 = document.getElementById("letter2");
+    d2.setAttribute("style","color:white");
 
 
 
