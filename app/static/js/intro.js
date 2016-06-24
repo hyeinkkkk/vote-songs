db727d6ec4f3eb9e8042c0b859361967e7f13b13
hyeinkkkk <hyein826@gmail.com>
2016-06-24 10:25:53 +0900

    내가 잘못했네

--- app/static/js/intro.js
+++ app/static/js/intro.js
@@ -1,4 +1,4 @@
-songApp.controller('IntroControllor', function($scope,$http,$location,)
+songApp.controller('IntroControllor', function($scope,$http,$location)
 {
     $scope.nextPage = function(){
         $location.path("/personal-data");
